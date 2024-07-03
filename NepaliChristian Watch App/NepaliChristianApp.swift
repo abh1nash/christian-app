bf4b870ca7f4e62d5df78b04cdfa78c18037c07c
Abhinash Khatiwada <me@abhinash.net>
2024-07-03 09:23:11 +0545

    Initial Commit

---
+++ NepaliChristian Watch App/NepaliChristianApp.swift
@@ -0,0 +1,17 @@
+//
+//  NepaliChristianApp.swift
+//  NepaliChristian Watch App
+//
+//  Created by Abhinash Khatiwada on 03/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct NepaliChristian_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
